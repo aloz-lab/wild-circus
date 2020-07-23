b71cd865e3a4a9c3565027baee90bccccccd29cd
aloz-lab <alozier45@gmail.com>
2020-07-23 21:31:09 +0200

    add resources

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -85,12 +85,14 @@ class CommentController extends AbstractController
      */
     public function delete(Request $request, Comment $comment): Response
     {
+        $show = $comment->getPerformance();
+        $id = $show->getId();
         if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($comment);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('comment_index');
+        return $this->redirectToRoute('show_show', ['id' => $id]);
     }
 }
